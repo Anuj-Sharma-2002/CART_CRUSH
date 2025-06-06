5794cc997a284eaaadd870b4ecc16b8a7ddf6f19
Anuj Sharma <s16anuj@gmail.com>
2025-06-06 23:12:36 +0530

    Adding Product.java in my model class and Creating Productrepository

---
+++ Shopping_Cart/src/main/java/com/ecom/model/Product.java
@@ -0,0 +1,89 @@
+package com.ecom.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+	@Column(length=500)
+    private String title;
+    
+	@Column(length=5000)
+    private String description;
+    
+    private Double price;
+    
+    private int stock;
+    
+    private String image;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
+}
